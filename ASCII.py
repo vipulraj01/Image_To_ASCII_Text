f39eb06fb4da5790e3d681b9b04e735d4434da4f
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-09-06 13:53:49 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,7 +1,17 @@
+# Deserialize from PIL import image
 from PIL import Image
+
+# Open walterwhite .jpg image
 Image.open(r"walterwhite.jpg")
 
+# Import pywhatkit.
 import pywhatkit
+
+# convert walterwhite .jpg to ascii art
 pywhatkit.image_to_ascii_art(r"walterwhite.jpg","walterwhite")
+
+# Read walterwhite. txt file
 read_file= open("walterwhite.txt","r")
+
+# Print the contents of a file to stdout
 print(read_file.read())
